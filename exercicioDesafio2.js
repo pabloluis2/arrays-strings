f8c770c820c5191a4f2a8a2cd5254077b592e448
pabloluis2 <pabloluis2@gmail.com>
2024-10-04 21:24:55 -0300

    Commitando os exercicios da aula 9 pt.2

---
+++ exercicioDesafio2.js
@@ -0,0 +1,4 @@
+let frutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+let indiceFruta = frutas.indexOf("Abacaxi")
+
+console.log(`O índice da palavra abacaxi é: ${indiceFruta}\nE o tamanho do array é: ${frutas.length}`)
---
+++ exercicioNotion2.js
@@ -0,0 +1,4 @@
+const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length) 
+//SUBI NUM ÔNIBUS EM MIRROCOS 27
